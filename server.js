e33cb72072b7ccbeef0f3629c48699f2ef0f3338
williamconnorbean <beanw@myumanitoba.ca>
2022-05-08 22:27:57 -0500

    Configure heroku deployment

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const path = require('path');
 const cors = require('cors');
 
 require('dotenv').config();
 const PORT = process.env.PORT || 5000;
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(__dirname, express.static('static_source/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'static_source', 'build', 'index.html'));
+  });
+}
 
 app.use(cors());
 app.use(bodyParser.json());
