1bd69422804e4b9a8d624091e8260c510ed903ef
williamconnorbean <beanw@myumanitoba.ca>
2022-05-12 21:29:17 -0500

    View list of products
    - add material ui for fast frontend component creation
    - factor out product into a list component and individual product
      component

--- static_source/src/components/home/Home.js
+++ static_source/src/components/home/Home.js
@@ -1,26 +1,13 @@
-import React, { useEffect } from 'react';
-import { connect } from 'react-redux';
-import { getProducts } from '../../actions/product/getProducts';
+import React from 'react';
+import ProductList from '../product/ProductList';
 
-const Home = (props) => {
-  // on mount
-  useEffect(() => {
-    props.dispatch(getProducts());
-    // eslint-disable-next-line
-  }, []);
-
+const Home = () => {
   return (
     <div>
-      <h1>Testing</h1>
+      <h1>Shopify Backend Challenge</h1>
+      <ProductList />
     </div>
   );
 };
 
-const mapStateToProps = (state) => ({
-  // from redux
-  products: state.product.getProducts.products,
-  loading: state.product.getProducts.loading,
-  error: state.product.getProducts.error
-});
-
-export default connect(mapStateToProps)(Home);
+export default Home;
